c6cf306e20fa8b25719e9b0ba0cc139a0f2522b8
bigfa <jigoulee@gmail.com>
2016-09-06 23:35:34 +0800

    fixedL fixed Archive error

--- archive.php
+++ archive.php
@@ -3,7 +3,6 @@
         <?php
         the_archive_title( '<h1 class="page-title">', '</h1>' );
         the_archive_description( '<div class="taxonomy-description">', '</div>' );
-        echo wp_term_like();
         ?>
     </header>
     <div class="postlists">
